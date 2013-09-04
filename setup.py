bb69e44c7fe6d8dc3c58f6ea8d2ee14fa458ceb5
Bryce Boe <bbzbryce@gmail.com>
2013-09-03 22:22:11 -0700

    Make package `kelp` and add appropriate setup.py file.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'hairball>=0.1rc3'
+    ]
+
+setup(name='kelp',
+      version='0.0',
+      description='kelp',
+      long_description=README,
+      classifiers=["Programming Language :: Python"],
+      author='Charlotte Hill',
+      author_email='charlotte@hillnetwork.com',
+      url='https://github.com/charlottehill/KelpPlugin',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      [console_scripts]
+      kelp = kelp.offline:main
+      """,
+      )
