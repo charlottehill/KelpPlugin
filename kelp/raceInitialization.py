a1feb7bf0eb79001b303b3961419ba1d6e0a85c7
Charlotte Hill <charlotte@hillnetwork.com>
2013-10-11 10:00:00 -0700

    Allows more block types
    Allows for block types for initializing the cat and rooster

--- kelp/raceInitialization.py
+++ kelp/raceInitialization.py
@@ -7,14 +7,14 @@ import os
 import sys
 import kurt
 
-'''How to run this plugin:                                                                                                 
-        hairball -k <path>/octopi.py -d <folder where sequenceViewer is> -p sequenceViewer.Sequence test.sb                
-        For example, if `octopi.py` and sequenceViewer are both in the directory where you are:                            
-    hairball -k octopi.py -d . -p sequenceViewer.Sequence test.sb                                                          
-    if sequenceViewer is in your directory but octopi.py is right outside of it:                                           
-    hairball -k ../octopi.py -d . -p sequenceViewer.Sequence test.sb                                                       
-    If they're both right outside of it:                                                                                   
-    hairball -k ../octopi.py -d .. -p sequenceViewer.Sequence test.sb                                                      
+'''How to run this plugin:
+        hairball -k <path>/octopi.py -d <folder where sequenceViewer is> -p sequenceViewer.Sequence test.sb
+        For example, if `octopi.py` and sequenceViewer are both in the directory where you are:
+    hairball -k octopi.py -d . -p sequenceViewer.Sequence test.sb
+    if sequenceViewer is in your directory but octopi.py is right outside of it:
+    hairball -k ../octopi.py -d . -p sequenceViewer.Sequence test.sb
+    If they're both right outside of it:
+    hairball -k ../octopi.py -d .. -p sequenceViewer.Sequence test.sb
 '''
 
 BASE_PATH = './results'
@@ -25,10 +25,10 @@ class raceInitialization(KelpPlugin):
     def __init__(self):
         super(raceInitialization, self).__init__()
 
-        """Returns a dictionary of the scripts.                                                
-        Keys: start events                                                                     
-        Values: another dictionary                                                             
-        Keys: sprite names                                                                     
+        """Returns a dictionary of the scripts.
+        Keys: start events
+        Values: another dictionary
+        Keys: sprite names
         Values: that sprite's scripts for this start event ."""
 
     def analyze(self, scratch):
@@ -43,36 +43,59 @@ class raceInitialization(KelpPlugin):
                     if not isinstance(script, kurt.Comment):
                         if KelpPlugin.script_start_type(script) == self.HAT_GREEN_FLAG:
                             scripts[sprite.name].add(script)
-        
+
         #initialize boolean initialization variables to False
         catPos = False
         catSize = False
         roosterPos = False
         roosterOrien = False
+        roosterSet = False
+        catSet = False
 
         #access the Cat's blocks
         for script in scripts['Cat']:
             for block in script:
-                # after convert() blockType is string of general category block fits into
-                blockType = block.type.convert()
-                if blockType.command == 'setSizeTo:':
+                if block.type.text == 'set size to %s%%':
                     if block.args[0] == 100:
                         catSize = True
-                elif blockType.command == 'gotoX:y:': 
-                    if block.args[0] >= 131:
+                elif block.type.text == 'go to x:%s y:%s':
+                    if block.args[0] >= 145:
                         catPos = True
-        
+                elif block.type.text == 'set x to %s':
+                    if block.args[0] >= 145:
+                        if catSet: # already set y
+                            catPos = True
+                        else:
+                            catSet = True
+                elif block.type.text == 'set y to %s':
+                    if catSet: # already set x
+                        catPos = True
+                    else:
+                        catSet = True
+
         #access the Rooster's blocks
         for script in scripts['Rooster']:
             for block in script:
-                # after convert() blockType is string of general category block fits into
-                blockType = block.type.convert()
-                if blockType.command == 'pointTowards:':
+                if block.type.text == 'point towards %s':
                     if block.args[0] == 'finish line':
                         roosterOrien = True
-                elif blockType.command == 'gotoX:y:': 
-                    if block.args[0] >= 131:
+                elif block.type.text == 'point in direction %s':
+                	if block.args[0] == -90:
+                		roosterOrien = True
+                elif block.type.text == 'go to x:%s y:%s':
+                    if block.args[0] >= 145:
                         roosterPos = True
+                elif block.type.text == 'set x to %s':
+                    if block.args[0] >= 145:
+                        if roosterSet: # already set y
+                            roosterPos = True
+                        else:
+                            catSet = True
+                elif block.type.text == 'set y to %s':
+                    if roosterSet: # already set x
+                        roosterPos = True
+                    else:
+                        roosterSet = True
 
         return {'Cat': catPos and catSize, 'Rooster': roosterPos and roosterOrien}
 
